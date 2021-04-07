d1cc46fb65a349d24455f4529caea6eb1afecd03
Gabriel Slama <gabriel.slama@akqa.com>
2021-04-07 14:08:20 +0200

    Remove analyze true rule

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    analyze: true,
     publicPath: process.env.NODE_ENV === "development" ? "_nuxt" : "/",
     filenames: {
       app: ({ isDev, isModern }) =>
